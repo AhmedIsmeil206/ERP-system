55d2d61fb3eadec6309cfcc956bf958b39f6a130
AhmedIsmeil206 <120604148+AhmedIsmeil206@users.noreply.github.com>
2024-09-07 22:54:01 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import AppRoutes from './routes/AppRoutes';
+import Navbar from './components/Navbar';
+import AuthProvider from './context/AuthContext';
+import { FinanceProvider } from './context/FinanceContext'; // Import FinanceProvider
+import './styles/tailwind.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <AuthProvider>
+        <FinanceProvider> {/* Wrap with FinanceProvider */}
+          <Navbar />
+          <AppRoutes />
+        </FinanceProvider>
+      </AuthProvider>
+    </Router>
   );
 }
 
