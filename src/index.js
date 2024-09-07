296b8415adc1bd9155226c8b50dfcfc4f6e06ab1
AhmedIsmeil206 <120604148+AhmedIsmeil206@users.noreply.github.com>
2024-09-07 22:54:27 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { InventoryProvider } from './context/InventoryContext';
+import { OrderProvider } from './context/OrderContext';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <InventoryProvider>
+      <OrderProvider>
+        <App />
+      </OrderProvider>
+    </InventoryProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
